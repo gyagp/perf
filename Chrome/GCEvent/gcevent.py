7cdac8b1d4b86dc33b0739aba39ad57565f6626a
Yang Gu <yang.gu@intel.com>
2013-03-04 15:35:10 +0800

    Update gcevent.py

--- Chrome/GCEvent/gcevent.py
+++ Chrome/GCEvent/gcevent.py
@@ -3,6 +3,32 @@ import sys
 import os
 import re
 
+INDEXSTARTTIME = 0;
+INDEXSIZE = 1;
+INDEXREASON = 2;
+INDEXENDTIME = 3;
+
+
+def formatTime(time, n):
+    return str(round(time, n));
+
+def printResult(result):
+    number = len(result);
+    for i in range(0, number):
+        print "Start: " + formatTime(float(result[i][INDEXSTARTTIME]) / 1000, 2) + "s";
+        print "Duration: " + formatTime(float(result[i][INDEXENDTIME]) - float(result[i][INDEXSTARTTIME]), 3) + "ms";
+
+        if int(result[i][INDEXSIZE]) > 1048576:
+            print "Size: " + str('%.2f'%(float(result[i][INDEXSIZE]) / 1048576)) + "MB";
+        elif int(result[i][INDEXSIZE]) > 1024:
+            print "Size: " + str('%.2f'%(float(result[i][INDEXSIZE]) / 1024)) + "KB";
+        else:
+            print "Size: " + result[i][INDEXSIZE] + "B";
+
+        print "Reason: " + result[i][INDEXREASON];
+
+        print "=============================="; 
+
 def analyze():
     if len(sys.argv) < 2:
         print "You must designate a file";
@@ -15,9 +41,11 @@ def analyze():
     # stackTrace is optional
     patternExpect = re.compile('GCEvent');
     # Should not be greedy because there might be multiple GCEvent in a line
-    pattern = re.compile('"startTime":(\d+.\d+),.*?"data":{"usedHeapSizeDelta":(\d+),"reason":(.*?)},"endTime":(\d+.\d+),"type":"GCEvent"');
-        
+    pattern = re.compile('"startTime":(\d+.\d+),(?:"stackTrace":\[.*?\],)?"data":{"usedHeapSizeDelta":(\d+),"reason":(.*?)},"endTime":(\d+.\d+),"type":"GCEvent"');
+    
     startTime = 0;
+    gcResultRaw = [];
+    gcResult = [];    
     
     while 1:
         line = file.readline();
@@ -39,22 +67,39 @@ def analyze():
         match = pattern.findall(line);
         
         if len(match) != numberExpect:
-            print "Pattern is not correct for following line, so there might be some GCEvent is missed";
+            print "Pattern is not correct for following line, so there might be some GCEvent missed";
             print "The expected GCEvent number is " + str(numberExpect) + ", but the actual match is " + str(len(match));
             print line;
             return -1;
 
         for i in range(0, numberExpect):
-            gcStartTimeTemp = "%.0f" % (float(match[i][0]) - float(startTime));
-            gcSizeTemp = int(match[i][1]);
-            gcReasonTemp = match[i][2];
-            gcDurationTemp = "%.3f" % (float(match[i][3]) - float(match[i][0]));
-            
-            patternIdleNotification = re.compile('idle notification:');
-            if not patternIdleNotification.search(gcReasonTemp):
-                print gcStartTimeTemp, gcSizeTemp, gcReasonTemp, gcDurationTemp;
-                
-            
+            gcResultRaw.append([float(match[i][INDEXSTARTTIME]) - float(startTime), match[i][INDEXSIZE], match[i][INDEXREASON], float(match[i][INDEXENDTIME]) - float(startTime)]);
+
+            #patternIdleNotification = re.compile('idle notification:');
+            #if not patternIdleNotification.search(gcReasonTemp):
+            #    print gcStartTimeTemp, gcSizeTemp, gcReasonTemp;
+    
+    numberTotal = len(gcResultRaw);
+    if numberTotal == 0:
+        return 0;
+
+    index = 0;
+    gcResult.append([gcResultRaw[index][INDEXSTARTTIME], float(gcResultRaw[index][INDEXSIZE]), gcResultRaw[index][INDEXREASON], gcResultRaw[index][INDEXENDTIME]]);
+    for i in range(1, numberTotal):
+        diff = abs(float(gcResult[index][INDEXENDTIME]) - float(gcResultRaw[i][INDEXSTARTTIME]));
+        #print diff;
+        if  diff < 0: # need to do merge
+            gcResult[index][INDEXSIZE] = float(gcResult[index][INDEXSIZE]) + float(gcResultRaw[i][INDEXSIZE]);
+            gcResult[index][INDEXREASON] += ", " + gcResultRaw[i][INDEXREASON]
+            gcResult[index][INDEXENDTIME] = gcResultRaw[i][INDEXENDTIME];
+            #print "merge happened";
+        else:
+            index += 1;
+            gcResult.append([gcResultRaw[i][INDEXSTARTTIME], gcResultRaw[i][INDEXSIZE], gcResultRaw[i][INDEXREASON], gcResultRaw[i][INDEXENDTIME]]);
+    
+    #printResult(gcResultRaw);
+    printResult(gcResult);
+
     file.close();    
     
 if __name__ == "__main__":
